cb939652b758d46cb1b0eb15b13d0757305faf4c
Harsh Mohan <harshmohan813@gmail.com>
2022-06-04 14:29:10 +0100

    Add custom input

--- sorting/main.js
+++ sorting/main.js
@@ -1,14 +1,42 @@
 document.body.onload = addElements;
 
-function addElement(idName, height, index) {
+const update = document.querySelector("#update")
+const sort = document.querySelector("#sort")
+
+update.addEventListener("click", function () {
+    let rawValues = document.querySelector("#input").value
+    let values = rawValues.split(",").map(Number);
+    removeDivs()
+    let i=0;
+    values.forEach(element => {
+        addElement((element*10)+"px", i++)
+    });
+});
+
+sort.addEventListener("click", function () {
+    console.log("clicked");
+});
+
+let removeDivs = () => {
+    let i=0;
+    let tempDivSelector = document.querySelector("[index='"+(i++)+"']")
+    while(tempDivSelector != null) {
+        tempDivSelector.remove()
+        tempDivSelector = document.querySelector("[index='"+(i++)+"']")
+    }
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+function addElement(height, index) {
     var newDiv = document.createElement("div");
-    // var newContent = document.createTextNode("Hi there and greetings!");
-    // newDiv.appendChild(newContent);
     newDiv.setAttribute("class", "element")
-    newDiv.setAttribute("id", idName)
     newDiv.setAttribute("index", index)
     newDiv.setAttribute("location", index)
     newDiv.style.height = height
+    newDiv.style.backgroundColor = "rgb("+getRandomArbitrary(0,255)+", "+getRandomArbitrary(0,255)+", "+getRandomArbitrary(0,255)+")"
 
     var currentDiv = document.getElementById("elements");
     currentDiv.append(newDiv)
@@ -16,43 +44,24 @@ function addElement(idName, height, index) {
 
 function addElements() {
     let i=0;
-    addElement("red", "60px", i++);
-    addElement("blue", "30px", i++);
-    addElement("green", "40px", i++);
-    addElement("yellow", "50px", i++);
-
-    // anime({
-    //     targets: 'div.element#red',
-    //     translateY: [
-    //         {value: 200, duration: 500},
-    //         {value: 0, duration: 500},
-    //     ],
-    // });
-    // anime({
-    //     targets: 'div.element#red',
-    //     // translateX: [
-    //     //             {value: 0, duration: 500},
-    //     //             {value: 12*1, duration: 500},
-    //     //         ],
-    //     translateX: 12*1,
-    //     delay: 500,
-    //     // easing: 'easeInOutExpo'
-    //     // duration: 3000
-    // });
+    addElement("60px", i++);
+    addElement("30px", i++);
+    addElement("40px", i++);
+    addElement("50px", i++);
 }
 
 function swap(targetA, targetB) {
-    let a = document.getElementById(targetA).getAttribute("location")
-    let b = document.getElementById(targetB).getAttribute("location")
+    let a = document.querySelector("[index='"+targetA+"']").getAttribute("location")
+    let b = document.querySelector("[index='"+targetB+"']").getAttribute("location")
     move(targetA, b)
     move(targetB, a)
 }
 
 function move(targetName, targetLoc) {
-    document.getElementById(targetName).setAttribute("location", targetLoc)
+    document.querySelector("[index='"+targetName+"']").setAttribute("location", targetLoc)
     anime({
-        targets: 'div.element#'+targetName,
-        translateX: 12*targetLoc - 12*document.getElementById(targetName).getAttribute("index"),
+        targets: "[index='"+targetName+"']",
+        translateX: 12*targetLoc - 12*document.querySelector("[index='"+targetName+"']").getAttribute("index"),
         delay: 100,
     });
 }
